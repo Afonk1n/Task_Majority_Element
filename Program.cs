7dc0e67ba339b4e3b88f9584b72786b7a7eabdb8
Kirito <afona2001@mail.ru>
2023-10-17 20:07:36 +0500

    solution

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Majority_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 3, 4};
+            int res = 0, counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (counter == 0)
+                {
+                    res = nums[i];
+                }
+                counter = res == nums[i] ? counter + 1 : counter - 1;
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
